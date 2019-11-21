1cea6286694ea5746c2c7c1b153ea8ae44c86082
ALIEF ADITYA <aliefaditya39@gmail.com>
2019-11-22 00:32:19 +0700

    migration laravel added to created antrean as a patient

--- database/migrations/2019_11_21_163518_create_table_antrean.php
+++ database/migrations/2019_11_21_163518_create_table_antrean.php
@@ -15,9 +15,17 @@ class CreateTableAntrean extends Migration
     {
         //create the table
         Schema::create('antrean', function (Blueprint $table){
-           $table->increments('id'); 
+           $table->primary('IdAntrean');
+           $table->increments('IdAntrean')->autoIncrement();
+           $table->unique('nomor');
+           $table->date('created_at'); 
+           $table->timestamps()->nullable();
 
+           $table->foreign('nik_pasien')->references('nik')->on('pasien');
+           $table->foreign('jadwal_poli')->references('Idjadwal')->on('jadwal');
+           
         });
+
     }
 
     /**
